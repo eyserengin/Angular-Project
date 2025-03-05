339921c1c93f1e73d1468849bfa2dfbd63f6eafa
eyserengin <osmaneyserengin@ogr.eskisehir.edu.tr>
2025-03-05 15:27:56 +0300

    first day in my ıntern process

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,32 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+//type User = {
+//  id: string;
+// name: string;
+// avatar: string;
+//};
+
+interface User {
+  id: string;
+  name: string;
+  avatar: string;
+}
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+  @Input({ required: true }) user!: User;
+  @Output() selectUser = new EventEmitter<string>();
+
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar;
+  }
+
+  onSelectUser() {
+    this.selectUser.emit(this.user.id);
+  }
+}
